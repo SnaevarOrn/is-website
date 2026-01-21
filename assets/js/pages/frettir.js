e26b66a05fcf3fbad5acd84e5954679b1af5c460
SnaevarOrn <98105425+SnaevarOrn@users.noreply.github.com>
2026-01-21 13:56:45 +0000

    Update frettir.js

--- assets/js/pages/frettir.js
+++ assets/js/pages/frettir.js
@@ -12,7 +12,7 @@
     { id: "frettin",   label: "Fréttin",         domain: "frettin.is" },
     { id: "vb",        label: "Viðskiptablaðið", domain: "vb.is" },
     { id: "bb",        label: "Bæjarins Besta", domain: "bb.is" },
-    { id: "fiskifrettir", label: "Fiskifréttir",   domain: "fjolmidillinn.is" },
+    { id: "fiskifrettir", label: "Fiskifréttir",   domain: "fiskifrettir.is" },
     { id: "frjalsverslun", label: "Frjáls verslun",  domain: "fjolmidillinn.is" },
     { id: "grapevine", label: "Grapevine",       domain: "grapevine.is" },
   ];
